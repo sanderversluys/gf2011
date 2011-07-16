5448476f7ae6d83a644535c1b74c605104d06d09
Sander Versluys <versluyssander@gmail.com>
2011-07-16 14:59:59 +0200

    disable some stuff before release

--- android/src/be/niob/apps/gf2011/BaseActivity.java
+++ android/src/be/niob/apps/gf2011/BaseActivity.java
@@ -31,7 +31,7 @@ public abstract class BaseActivity extends Activity {
 	    	actionBar.setTitle(R.string.app_name);
 	    	actionBar.setHomeAction(new IntentAction(this, new Intent(this, HomeActivity.class), R.drawable.ic_title_home));
 	    	//actionBar.addAction(new IntentAction(this, new Intent(this, DaysActivity.class), R.drawable.ic_title_today));
-	    	actionBar.addAction(new IntentAction(this, new Intent(this, DaysActivity.class), R.drawable.ic_title_search));
+	    	//actionBar.addAction(new IntentAction(this, new Intent(this, DaysActivity.class), R.drawable.ic_title_search));
 	    	//actionBar.addAction(new ToastAction());
     	}
     }
